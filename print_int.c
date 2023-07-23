01985a09e949cd1ea2b8376ddeb1f2f2bd36e4f0
Deolahh <Deolahh>
2023-07-23 03:58:28 -0700

    print int

---
+++ print_int.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_con_spec - handles %d and %i conversion
+ * @format: pointer to char d and i
+ * Return: count
+ */
+
+int print_con_spec(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	int i = 0;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			{
+				int num = va_arg(args, int);
+
+				putchar(num + '0');
+				count++;
+				i++;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
