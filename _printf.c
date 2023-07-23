6157858d86730d01eb575b5b02c905d3d200cec0
Habeetech <oluhabnoni@gmail.com>
2023-07-23 15:05:24 -0700

    task 1

--- _printf.c
+++ _printf.c
@@ -1,11 +1,22 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _putchar - Custom putchar function to write a single character to stdout.
+ * @c: The character to be written.
+ * Return: On success, 1 is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 /**
  * _printf - produce outputs according to format
  * @format: type of the string
  * Return: number of characters printed (excluding the null byte)
  * Write output to standard output
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -13,39 +24,36 @@ int _printf(const char *format, ...)
 	int i = 0;
 
 	va_start(args, format);
-	while (format && format[i])
+	while (format && format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			switch (format[i])
+			if (format[i + 1] == 'c')
 			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-						write(1, &c, 1), count++, break;
-					}
-				case 's':
-					{
-						char *str = va_arg(args, char*);
-						int len = 0;
-				
-						while (str[len])
-							len++, write(1, str, len), count += len, break;
-					}
-				case '%':
-					{
-						write(1, "%", 1), count++, break;
-					}
-				default:
-					break;
+				_putchar(va_arg(args, int)), count++, i++;
 			}
+			else if (format[i + 1] == 's')
+			{
+				char *str = va_arg(args, char*);
+
+				if (str)
+				{
+					while (*str)
+					{
+						_putchar(*str), count++, str++;
+					}
+				} i++;
+			}
+				else
+				{
+					_putchar(format[i]), count++;
+				}
+			}
+			else
+			{
+				_putchar(format[i]), count++;
+			} i++;
 		}
-		else
-		{
-			write(1, &format[i], 1), count++;
-		} i++;
-	}
 	va_end(args);
-	return count;
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _putchar(char c);
 int _printf(const char *format, ...);
 int print_int(const char *format, ...);
 #endif
