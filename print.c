598633bf6114a5bab964136803e278d361193aa8
Habeetech <oluhabnoni@gmail.com>
2023-07-24 11:55:24 -0700

    printf

--- print.c
+++ print.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 /**
  * _putchar_char - outputs char
  * @c: A char
@@ -41,4 +42,35 @@ void _putchar_int(int num)
 	{
 		perror("_putchar_int");
 	}
+}
+void _putchar_char(char c);
+void _putchar_uint(unsigned int num) {
+    char buffer[20];
+    snprintf(buffer, sizeof(buffer), "%u", num);
+    _putchar_str(buffer);
+}
+    void _putchar_hex(unsigned int num, bool uppercase) {
+    char buffer[20];
+    snprintf(buffer, sizeof(buffer), uppercase ? "%X" : "%x", num);
+    _putchar_str(buffer);
+}
+
+
+void _putchar_float(double num, int precision) {
+    char buffer[50];
+    snprintf(buffer, sizeof(buffer), "%.*f", precision, num);
+    _putchar_str(buffer);
+}
+void _putchar_escape(char c) {
+    switch (c) {
+        case '\n':
+            _putchar_str("\\n");
+            break;
+        case '\t':
+            _putchar_str("\\t");
+            break;
+        default:
+            _putchar_char(c);
+            break;
+    }
 }
